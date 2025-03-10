511f54cc308ffdfc4386081cfb9111be6293c47a
Mimbar Adi Nugraha <mimbar.muse@gmail.com>
2025-03-11 06:02:10 +0700

    init assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,20 @@
+import VueSweetalert2 from 'vue-sweetalert2';
+
+import BootstrapVueNext from 'bootstrap-vue-next';
+import vClickOutside from "click-outside-vue3";
+import VueApexCharts from "vue3-apexcharts";
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@vueuse/nuxt', '@nuxt/eslint'],
   css: [
-    // '@/assets/css/bootstrap.css',
-    // '@/assets/css/app.css',
-    // '@/assets/css/icons.css'
+    '@/assets/css/bootstrap.min.css',
+    '@/assets/css/app.min.css',
+    '@/assets/css/icons.min.css',
+    'sweetalert2/dist/sweetalert2.min.css',
+    'leaflet/dist/leaflet.css',
+    // '@/assets/scss/config/default/app.scss',
+    // '@/assets/scss/mermaid.min.css',
   ],
 })
